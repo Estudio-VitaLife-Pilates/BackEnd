cde599c4b17f8f7c4ef51b263817d5b2312344d5
Eduardo Klein de Figueiredo <eduardo.figueiredo@sptech.school>
2026-04-02 17:40:58 -0300

    dependências do projeto

---
+++ pilates/src/test/java/almeida/thais/pilates/PilatesApplicationTests.java
@@ -0,0 +1,13 @@
+package almeida.thais.pilates;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PilatesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
