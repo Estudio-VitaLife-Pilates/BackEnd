878f045f82ff2f1729043e662f516e1c4f841058
Bruno Oliveira <bruno.doliveira@sptech.school>
2026-04-12 03:19:52 -0300

    Inicializando API Rest

---
+++ src/test/java/com/pilates/thais/almeida/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pilates.thais.almeida;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
